33b871262d3d42b603b2e811798d408fce64ea1c
Jerry Mailloux <jerrymailloux@gmail.com>
2023-03-25 12:15:18 -0400

    Added documentation. Fixed formatting

--- cmd_math.py
+++ cmd_math.py
@@ -1,7 +1,9 @@
 import random
 from time import time
 
-def get_ans(num1, num2):
+def get_ans(num1:int, num2:int) -> int:
+  '''Prompts user to enter the answer for num1 * num 2.  Returns user inputted answer as int
+  '''
   ans = None
   while(ans is None):
     try:
@@ -11,22 +13,22 @@ def get_ans(num1, num2):
   return ans
 
 def main():
-    start = time()
-    total = 0
-    correct = 0
-    duration = 0
-    random.seed()
-    while(duration < 10):
-      num1 = random.randint(0, 10)
-      num2 = random.randint(0, 10)
-      user_ans = get_ans(num1, num2)
-      total += 1
-      if(user_ans == num1 * num2):
-         correct += 1
-      percent = correct * 100 / total
-      now = time()
-      duration = (now-start)/60
-      print(f'{duration:.2f} min: {correct}/{total}, {percent:.1f}%')
+  start = time()
+  total = 0
+  correct = 0
+  duration = 0
+  random.seed()
+  while(duration < 10):
+    num1 = random.randint(0, 10)
+    num2 = random.randint(0, 10)
+    user_ans = get_ans(num1, num2)
+    total += 1
+    if(user_ans == num1 * num2):
+        correct += 1
+    percent = correct * 100 / total
+    now = time()
+    duration = (now-start)/60
+    print(f'{duration:.2f} min: {correct}/{total}, {percent:.1f}%')
 
 
 if __name__ == "__main__":
