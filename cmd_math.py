8f1abacae86cb9cf50df1cc71abdf6a44e9c2abe
Jerry Mailloux <jerrymailloux@gmail.com>
2023-04-12 21:01:43 -0400

    Added division questions

--- cmd_math.py
+++ cmd_math.py
@@ -1,38 +1,51 @@
 import random
 from time import time
 
-def get_ans(num1:int, num2:int) -> int:
-  '''Prompts user to enter the answer for num1 * num 2.  Returns user inputted answer as int
+def get_ans(num1:int, num2:int, op:int) -> int:
+  '''Prompts user to enter the answer to a question.
+     Returns user inputted answer as int
   '''
   ans = None
   while(ans is None):
     try:
-      ans = int(input(f'What is {num1} * {num2}? ' ))
+      if(op == 0):
+        ans = int(input(f'What is {num1} * {num2}? ' ))
+      else:
+        dividend = num1 * num2
+        ans = int(input(f'What is {dividend} / {num1}? ' ))
     except ValueError:
       print("Please enter a number!")
   return ans
 
 def main():
-
   total = 0
-  correct = 0
+  num_correct = 0
   duration = 0
+
   random.seed()
   while(duration < 10):
-    num1 = random.randint(0, 10)
+    correct = False
+    num1 = random.randint(1, 10)
     num2 = random.randint(0, 10)
-    user_ans = get_ans(num1, num2)
+    op = random.randint(0,1)
+    user_ans = get_ans(num1, num2, op)
     total += 1
     if total == 1:
       start = time()
-    if(user_ans == num1 * num2):
-        correct += 1
+    if(op == 0):
+      if(user_ans == num1 * num2):
+        num_correct += 1
+        correct = True
     else:
-       print('Wrong!!!!!\n')
-    percent = correct * 100 / total
+      if(user_ans == num2):
+        num_correct += 1
+        correct = True
+    if not correct:
+      print('Wrong!!!!!\n')
+    percent = num_correct * 100 / total
     now = time()
     duration = (now-start)/60
-    print(f'{duration:.2f} min: {correct}/{total}, {percent:.1f}%')
+    print(f'{duration:.2f} min: {num_correct}/{total}, {percent:.1f}%')
 
 
 if __name__ == "__main__":
